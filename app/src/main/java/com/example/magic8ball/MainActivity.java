e3bc2930707906606d3b84de42be6587896985dd
paulkirwan6 <39994963+paulkirwan6@users.noreply.github.com>
2019-06-11 12:33:37 +0100

    Working version - 20 messages added.

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -28,12 +28,11 @@ public class MainActivity extends AppCompatActivity
         //Force portrait orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-
-
+        //8Ball is tapped
         onTap();
     }
 
-    //User taps the 8ball
+    //User tapping the 8ball is a valid input
     public void onTap()
     {
         ImageButton imageButton = findViewById(R.id.imageButton);
@@ -49,48 +48,65 @@ public class MainActivity extends AppCompatActivity
     //Change text with new answer
     public void onInput()
     {
-        hideEight();
-        final TextView answer = findViewById(R.id.answer);
-        answer.setText(generateAnswer());
-    }
-
-    public void hideEight()
-    {
+        //Hide original view first
         TextView eight = findViewById(R.id.eight);
-        //Toggle
         if (eight.getVisibility() == View.VISIBLE)
             eight.setVisibility(View.INVISIBLE);
+
+        //Display new answer
+        final TextView answer = findViewById(R.id.answer);
+        answer.setText(generateAnswer());
     }
 
     //Select random answer to return
     public String generateAnswer()
     {
         //Random int
-        int random =(int) (Math.random()*10+1);
+        int random =(int) (Math.random()*20+1);
 
-        //1 random answer out of 10 is picked
+        //1 random answer out of 20 is picked
         switch (random)
         {
             case 1:
-                return "1";
+                return "It is certain.";
             case 2:
-                return "2";
+                return "It is decidedly so.";
             case 3:
-                return "3";
+                return "Without a doubt.";
             case 4:
-                return "4";
+                return "Yes - definitely.";
             case 5:
-                return "5";
+                return "You may rely on it.";
             case 6:
-                return "6";
+                return " As I see it, yes.";
             case 7:
-                return "7";
+                return " Most likely.";
             case 8:
-                return "8";
+                return "Outlook good.";
             case 9:
-                return "9";
+                return "Yes.";
             case 10:
-                return "0";
+                return "Signs point to yes.";
+            case 11:
+                return "Reply hazy, try again.";
+            case 12:
+                return "Ask again later.";
+            case 13:
+                return "Better not tell you now.";
+            case 14:
+                return "Cannot predict now.";
+            case 15:
+                return "Concentrate and ask again.";
+            case 16:
+                return "Don't count on it.";
+            case 17:
+                return "My reply is no.";
+            case 18:
+                return "My sources say no.";
+            case 19:
+                return "Outlook not so good.";
+            case 20:
+                return "Very doubtful.";
         }
         //else
         return "";
